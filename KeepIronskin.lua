489a282382db5fa0430258129cecc636415d5786
AgMonk <31527430+AgMonk@users.noreply.github.com>
2018-04-10 01:28:50 +0800

    2.0 alpha

--- KeepIronskin.lua
+++ KeepIronskin.lua
@@ -1,7 +1,7 @@
 --------设置-----------
 local config = {
 tt = 2, --默认报告间隔
-MaxStaggerTaken = 80, --醉拳承受率上限,高于此比例时提示使用更多活血
+MaxStaggerTaken = 80, --醉拳承受率上限
 Maxdot = 400*1000, --dot上限，当每跳dot伤害大于该值时视为dot伤害过高
 whitelist = { --白名单，受到这些伤害时不会报告
 	[209858]=true, --死疽
@@ -13,9 +13,9 @@ whitelist = { --白名单，受到这些伤害时不会报告
 --以下为各功能开关，true=开，false=关	
 Enable = true, --总开关
 w = false,			 	--密语开关，如果打开，将会抄送一份报告密语给目标
-isbapply = false, 	--是否报告铁骨上身
+isbapply = false, 		--是否报告铁骨上身
 isbfade = true,			--是否报告铁骨消失
-pfblvl = true, 	--是否报告活血使用水平 如果醉拳承受低于上限值10%以上则提示打的不错.
+pfblvl = true, 			--是否报告活血使用水平 如果醉拳承受超过醉拳承受率上限提示使用更多活血
 casts = false,			--是否报告技能使用情况
 hit = true, 			--是否报告断铁骨时被命中
 purify = true, 			--是否报告裸活血
@@ -51,7 +51,7 @@ local function CreateTable() --建档
 	["dot"] = 0, --醉拳承受
 	["pool"] = 0,	--醉拳吸收
 	["hits"] = 0,	--被命中次数
-	["isb"] = 0, --断铁骨命中次数
+	["isb"] = 0, --断铁骨命中次数	
 	["purify"] = 0, --裸活血次数
 	["combatstart"] = 0, --战斗开始
 	["combatend"] = 0,	--战斗结束
@@ -79,10 +79,11 @@ local function PrintTable(name,tab)
 		q = math.floor(q*1000)/10
 				
 	print("玩家："..name.."的本次战斗结束","战斗时长："..shortnum(tab.combatend-tab.combatstart).."秒")
-	print("醉拳承受："..shortnum(tab.dot),"醉拳吸收："..shortnum(tab.pool),"活血率："..p.."%")
-	print("被命中次数："..tab.hits,"其中断铁骨被命中次数："..tab.isb,"覆盖率："..q.."%")
-	print("裸活血次数："..tab.purify)
-	
+	--print("醉拳承受："..shortnum(tab.dot),"醉拳吸收："..shortnum(tab.pool),"活血率："..p.."%")
+	print("活血率："..p.."%")
+	--print("被命中次数："..tab.hits,"其中断铁骨被命中次数："..tab.isb,"覆盖率："..q.."%")
+	print("铁骨覆盖率："..q.."%")
+	print("裸活血次数："..tab.purify)	
 	print("施法统计：")
 	for id,count in pairs(tab.casts) do 		
 		print(GetSpellLink(id),count)
@@ -111,12 +112,24 @@ local function trigger(self,event,timestamp,eventtype,hideCaster,srcGUID, srcNam
 	if not config.Enable then return end --总开关
 	if UnitPosition("player") then return end --仅在副本中工作
 	
+	if eventtype == "SPELL_AURA_REMOVED" and other[1]==buff.isb  then --铁骨消失
+
+		if config.isbfade then report(dstName.."已经失去"..GetSpellLink(spell.isb) .."，治疗注意！",false)  end 
+	end	
+	if eventtype == "SPELL_AURA_APPLIED" and other[1]==buff.isb  then --铁骨上身
+
+		if  config.isbapply  then report(dstName.."已经获得"..GetSpellLink(spell.isb) .."，稳如POI！",false) end 
+	end		
+	
 	if eventtype == "SPELL_CAST_SUCCESS" and data[srcName] and data[srcName].casts[other[1]] then	--施法次数统计
 		if not data[srcName] then  data[srcName]=CreateTable() data[srcName].combatstart = GetTime() end --建档
 		data[srcName].casts[other[1]] = data[srcName].casts[other[1]] + 1
-		if other[1] == spell.pfb and not OnAura(srcName,buff.isb) then --裸活血次数
-			data[srcName].purify = data[srcName].purify + 1
-			if config.purify then report(srcName.."在断铁骨下使用了"..GetSpellLink(spell.pfb).."请覆盖"..GetSpellLink(spell.isb) ,false,srcName)  end
+		if other[1] == spell.pfb  then --裸活血次数
+			local isbdura = {UnitAura(srcName,GetSpellInfo(215479))}
+			if isbdura[6] <=2 then 
+				data[srcName].purify = data[srcName].purify + 1 
+				if config.purify then report(srcName.."在断铁骨下使用了"..GetSpellLink(spell.pfb).."请覆盖"..GetSpellLink(spell.isb) ,false,srcName)  end
+			end 
 		end 
 	end 
 	
@@ -163,7 +176,7 @@ local function trigger(self,event,timestamp,eventtype,hideCaster,srcGUID, srcNam
 	if GetTime()>checkpool+3 then  --每3秒检查是否脱战
 		checkpool = GetTime()
 		for name,tab in pairs(data) do  --遍历表，查看酒仙酒池是否归零
-			if UnitStagger(name)==0 and tab.pool>0 then --如果目标酒池归零，视为脱战
+			if UnitStagger(name)==0 and tab.pool>0 and config.casts then --如果目标酒池归零，视为脱战
 			--开始计算战斗时长，铁骨覆盖率，活血率，技能使用情况
 				
 				PrintTable(name,tab)
@@ -172,12 +185,7 @@ local function trigger(self,event,timestamp,eventtype,hideCaster,srcGUID, srcNam
 		end 
 	end
 	
-	if eventtype == "SPELL_AURA_REMOVED" and other[1]==buff.isb and InCombatLockdown()  then 
-		if config.isbfade then report(dstName.."已经失去"..GetSpellLink(spell.isb) .."，治疗注意！",false)  end 
-	end	
-	if eventtype == "SPELL_AURA_APPLIED" and other[1]==buff.isb and  InCombatLockdown() then 
-		if  config.isbapply  then report(dstName.."已经获得"..GetSpellLink(spell.isb) .."，稳如POI！",false) end 
-	end			
+		
 	
 end 
 
